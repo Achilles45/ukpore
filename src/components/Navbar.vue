025597ec1122379245671166196c7675f79bacf8
Achilles45 <achillesusuoyibo7@gmail.com>
2020-06-26 10:49:29 +0100

    More fixes to the navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -134,7 +134,10 @@ export default {
 //MEDIA QUERIES
 @media only screen and (max-width: 1100px){
     .navbar__wrapper{
-        padding: 2rem 0 !important;
+        padding: 1rem 0 !important;
+    }
+    .navbar__holder{
+        padding: 0 1.2rem !important;
     }
     .logo__img{
         max-width: 200px !important;
